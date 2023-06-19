4d72a2c6d01c61ab0ac6e70f79bdf1c5b8dd217d
higo <higoedu@gmail.com>
2023-06-19 19:49:15 -0300

    prompt convertendo uma string para número
    prompt convertendo uma string para número

---
+++ p_03_prompt_convertendo_uma_String_para_numero/main.js
@@ -0,0 +1,13 @@
+let primeiroNome = prompt('Digite o seu primeiro nome:');
+let ultimoNome = prompt('Digite o seu último nome:');
+let idade = parseInt(prompt('Digite a sua idade:'));
+
+const numero = 2;
+
+//numero = 4;
+
+idade = idade + numero;
+
+console.log(primeiroNome);
+console.log(ultimoNome);
+console.log(idade);
